3bc6c4e235d8b4c73f8bad523fd863b35086b51a
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-06-13 17:08:23 +0200

    auto commit

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwolf <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/08 16:47:34 by jwolf             #+#    #+#             */
+/*   Updated: 2018/06/04 10:38:08 by jwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ret;
+	size_t	i;
+	size_t	f;
+
+	f = ft_strlen((char *)s1);
+	i = 0;
+	MALLCHECK_N((ret = ft_memalloc(sizeof(char) * (f + 1))));
+	while (i < f)
+	{
+		ret[i] = s1[i];
+		i++;
+	}
+	ret[i] = '\0';
+	return (ret);
+}
