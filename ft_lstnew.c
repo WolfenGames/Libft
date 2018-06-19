5d561c2458d9749c1677e53a5b19cfe7dfa103cf
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-06-19 11:43:18 +0200

    Final fix:: I hope

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -15,7 +15,7 @@
 t_list	*ft_lstnew(const void *content, size_t content_size)
 {
 	t_list	*ret;
-	
+
 	MALLCHECK_N((ret = (t_list *)malloc(sizeof(t_list))));
 	if (content)
 	{
--- ft_strdup.c
+++ ft_strdup.c
@@ -21,11 +21,6 @@ char	*ft_strdup(const char *s1)
 	f = ft_strlen((char *)s1);
 	i = 0;
 	MALLCHECK_N((ret = (char *)ft_memalloc(sizeof(char) * (f + 1))));
-	while (i < f)
-	{
-		ret[i] = s1[i];
-		i++;
-	}
-	ret[i] = '\0';
+	ret = ft_strcpy(ret, s1);
 	return (ret);
 }
